306465519f2615c8bb79fe44ad2b46cdbd1b87f1
Ken Huang <bluetch@gmail.com>
2022-11-20 18:29:55 +0800

    create branch: refactor

--- homework_19/pages/posts/index.tsx
+++ homework_19/pages/posts/index.tsx
@@ -1,50 +1,50 @@
 import Link from 'next/link';
 import styles from 'styles/main.module.scss';
 
-interface Posts{
-    posts:{
-        map: any
-        id:string,
-        title:string,
-        body:string
-    }
+interface Posts {
+  posts: {
+    map: any
+    id: string,
+    title: string,
+    body: string
+  }
 }
 
-const PostList = ({posts}: Posts) =>{
-    return(
-        <div className="container">
-            <div className={styles.card}>
-                <h1>List of Posts</h1>
-                {posts.map((post: any)=>{
-                    return(
-                        <div key={post.id}>
-                            <Link href={`poosts/${post.id}`}>
-                            <p>
-                            {post.id}.{post.title}
-                            </p>
-                            </Link>
-                            <hr />
-                            </div>
-                    )
-                })}
-
+const PostList = ({ posts }: Posts) => {
+  return (
+    <div className="container">
+      <div className={styles.card}>
+        <h1>List of Posts</h1>
+        {posts.map((post: any) => {
+          return (
+            <div key={post.id}>
+              <Link href={`posts/${post.id}`}>
+                <p>
+                  {post.id}.{post.title}
+                </p>
+              </Link>
+              <hr />
             </div>
+          )
+        })}
 
-        </div>
-    )
+      </div>
+
+    </div>
+  )
 }
 
 export default PostList;
 
-export const getStaticProps = async () =>{
-const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-const data = await response.json()
- 
- return{
-    props:{
-        posts: data
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await response.json()
+
+  return {
+    props: {
+      posts: data
     }
- }
+  }
 
 }
 
